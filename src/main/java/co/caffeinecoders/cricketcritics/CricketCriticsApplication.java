1c16f4b18daf5d667be34aa94dfef92f072a485e
floralgreen <carducci.federico20@gmail.com>
2024-03-12 22:42:08 +0100

    first commit

---
+++ src/main/java/co/caffeinecoders/cricketcritics/CricketCriticsApplication.java
@@ -0,0 +1,13 @@
+package co.caffeinecoders.cricketcritics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketCriticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketCriticsApplication.class, args);
+	}
+
+}
